17838bde3e58074741e34b1c4ede58fe6c23e7f0
Aleksei <suslov.aleksey.oo7@gmail.com>
2021-04-09 21:04:34 +0300

    Initial Commit

---
+++ ContactsSwiftUI/ContactsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsSwiftUIApp.swift
+//  ContactsSwiftUI
+//
+//  Created by ALEKSEY SUSLOV on 09.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct ContactsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
